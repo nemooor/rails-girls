c0d44fa01cd6636b3eaa700b2fa146ea12b35395
nemor <nemo_r@pepabo.com>
2023-04-08 16:58:40 +0900

    カテゴリーが表示されるようにした

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -65,6 +65,6 @@ class IdeasController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def idea_params
-      params.require(:idea).permit(:name, :description, :picture)
+      params.require(:idea).permit(:name, :description, :picture, :category)
     end
 end
